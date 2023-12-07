71976acb551640a08a2f925a65f2a4c4020d5de6
Facundo Reiseng <facu_carhue@hotmail.com>
2023-12-07 00:39:35 -0300

    Get de turnos

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask,blueprints
 from controladores.medicos import medicos_bp
 from controladores.pacientes import pacientes_bp
 from controladores.agenda_medicos import agenda_bp
+from controladores.turnos import turnos_bp
 app = Flask(__name__)
 
 app.register_blueprint(medicos_bp)
 app.register_blueprint(pacientes_bp)
 app.register_blueprint(agenda_bp)
+app.register_blueprint(turnos_bp)
 
 if __name__=="__main__":
     app.run(debug=True)
